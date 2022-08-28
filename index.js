2678379d148e600f910c8ef87fb453ca6ff7ce81
andasan <andasan@gmail.com>
2022-08-28 09:57:33 -0700

    Setting PORT in dev environment to make sure the app listens on the specified port.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.status(200).send('Health Check');
 });
 
-const server = app.listen(3000, function () {
+const server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
